63db6ca5a56eb0d756a10670d5891675beb9719e
GhislainBisamaza <95704645+GhislainBisamaza@users.noreply.github.com>
2021-12-11 12:58:10 +0200

    Add files via upload

---
+++ keypad/keypad.ino
@@ -0,0 +1,90 @@
+
+#include <Keypad.h>
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A',},
+  {'4','5','6','B',},
+  {'7','8','9','C',},
+  {'*','0','#','D',},
+};
+byte rowPins[ROWS] = { 9, 8, 7, 6 };
+byte colPins[COLS] = { 13,12, 11, 10 }; 
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+char inputArray[4]; //password length
+char Main[4] = {'1','2','3','5'}; //main or master password
+char Guest[4] = {'1','2','3','4'}; //guest password
+int ledpinn=2;
+int ledpin=3; //led to register keypad pressses
+int buzzer=4; //led that registers correct password entry
+
+int i = 0;
+void setup()
+{
+Serial.begin(9600); //open serial port
+pinMode(ledpinn,OUTPUT);
+pinMode(ledpin, OUTPUT); //define led pin as output
+pinMode(buzzer, OUTPUT); //define led pin as output
+
+}
+
+void loop()
+{
+
+
+ char key = kpd.getKey();
+
+   if(key)
+	{
+
+	digitalWrite(buzzer, HIGH);
+	delay(100);
+	digitalWrite(buzzer, LOW);
+	delay(100);
+	inputArray[i] = key; 
+	i++;
+	Serial.println(key); 
+
+	if (key=='*')
+	  {
+	    Serial.println("Reset");
+	    i=0; 
+	  }
+
+	if (i == 4) 
+	  {
+	    {
+
+	    //match input array to password array
+
+		if (inputArray[0] == Guest[0] &&
+		inputArray[1] == Guest[1] &&
+		inputArray[2] == Guest[2] &&
+		inputArray[3] == Guest[3])
+		   {
+        digitalWrite(ledpin, HIGH);
+        digitalWrite(ledpinn, LOW);
+        
+	
+		   }
+
+	if (inputArray[0] == Main[0] &&
+		inputArray[1] == Main[1] &&
+		inputArray[2] == Main[2] &&
+		inputArray[3] == Main[3])
+		   {
+        digitalWrite(ledpinn, HIGH);
+        digitalWrite(ledpin, LOW);
+	
+		   }
+	    }
+	    {
+	    i=0; //reset i
+	    }
+	  }
+	}
+  }
+
+
+
---
+++ rfid/rfid.ino
@@ -0,0 +1,53 @@
+#include <AltSoftSerial.h>// Rx =8, Tx= 9
+AltSoftSerial altSerial;
+String tag;
+int forward=4;
+int reverse=5;
+int x;
+void setup()
+{
+ 
+  Serial.begin(9600);
+ 
+ altSerial.begin(9600);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+}
+
+char c;
+
+void loop(){
+    while(altSerial.available()>0){
+    c=altSerial.read();
+    tag += c;
+  }
+tag=tag.substring(0,13);
+while(tag.length()>0)
+{
+Serial.println(tag);
+
+
+if(tag=="00001610696F") {
+  x=0;
+  while(x<1){
+  digitalWrite(4,HIGH);
+digitalWrite(5,LOW);
+delay(5000);
+digitalWrite(4,LOW);
+ digitalWrite(5,HIGH);
+ delay(5000);
+ digitalWrite(5,LOW);
+ x+=1;
+ 
+}
+}
+tag="";
+delay(200);
+
+tag="";
+}
+delay(200);
+tag="";
+}
+
+
